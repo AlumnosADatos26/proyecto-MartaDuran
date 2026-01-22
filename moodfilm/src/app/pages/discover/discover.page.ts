f8125f76950e2b5078304fd607a6bd925df29c27
martadurangemes <marta.duran.gemes.alu@iesjulioverne.es>
2026-01-22 23:31:05 +0100

    Add files via upload

---
+++ moodfilm/src/app/pages/discover/discover.page.ts
@@ -0,0 +1,70 @@
+import { Component } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { MovieService } from 'src/app/services/MovieService';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-discover',
+  templateUrl: './discover.page.html',
+  styleUrls: ['./discover.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule],
+})
+
+export class DiscoverPage {
+
+  movies: any[] = [];     // lista de películas que se mostrarán 
+  page = 1;               // Página actual (para la paginación)
+  loading = true;         // controla el mensaje de carga
+  readonly IMAGE_BASE_URL = environment.tmdbImageUrl;
+
+  constructor(private movieService: MovieService, private router: Router) { }
+
+  ngOnInit() {
+    this.loadMovies();
+  }
+
+  getPosterUrl(posterPath: string | null): string {
+    if (!posterPath) {
+      return 'assets/img/no-poster.png'; // fallback
+    }
+    return `${this.IMAGE_BASE_URL}${posterPath}`;
+  }
+
+
+  //Carga las películas populares desde el servicio
+  async loadMovies() {
+    try {
+      this.loading = true;
+      const respuesta = await this.movieService.getPopularMovies(this.page);
+      // guardamos solo el array de resultados
+      this.movies = respuesta.results;
+
+    } catch (error) {
+      console.error('Error al cargar películas', error);
+    } finally {
+      this.loading = false;
+    }
+  }
+
+
+  //Al hacer click en una película:
+  //navegamos a la página de detalles pasando el ID por queryParams
+  verFicha(movie: any) {
+    this.router.navigate(
+      ['/movie-details'],
+      { queryParams: { id: movie.id } }
+    );
+  }
+
+  // Carga la siguiente página de películas
+  async loadMoreMovies() {
+    this.page++;          // pasamos a la siguiente página
+    await this.loadMovies();
+  }
+
+
+}
---
+++ moodfilm/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  tmdbApiKey: 'ef7e2eea6a70b4a7b8bb8c73cdf9d549',
+  tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageUrl: 'https://image.tmdb.org/t/p/w500'
+};
