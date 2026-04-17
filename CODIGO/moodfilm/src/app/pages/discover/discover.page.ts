100ca10a6ec68c35b6cc696e1a09c5d505e625e5
SuMartukiRechulona5555 <marta.duran.gemes.alu@iesjulioverne.es>
2026-04-17 18:43:53 +0200

    Implementación del botón del dado para sugerencias de películas en Descubre. Corregí problemas visuales donde el botón mostraba bordes blancos y cambios de color azul al pasar el cursor. También tuve que cambiar el diseño del modal varias veces ya que el póster me daba problemas a la hora del responsive, y se arregló un bug de estado que mantenía el modal abierto al navegar entre páginas.

--- CODIGO/moodfilm/src/app/pages/discover/discover.page.ts
+++ CODIGO/moodfilm/src/app/pages/discover/discover.page.ts
@@ -5,7 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MovieService } from 'src/app/services/MovieService';
 import { environment } from 'src/environments/environment';
-
+import { diceOutline } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
@@ -21,7 +22,19 @@ export class DiscoverPage {
   loading = true;         // controla el mensaje de carga
   readonly IMAGE_BASE_URL = environment.tmdbImageUrl;
 
-  constructor(private movieService: MovieService, private router: Router) { }
+  surpriseMovie: any = null;
+  readonly moodToGenres: any = {
+    feliz: [35, 10751, 16],
+    triste: [18, 10749],
+    emocionado: [28, 12, 878],
+    relajado: [16, 35],
+    miedo: [27, 53]
+  };
+  isSurprising = false;
+
+  constructor(private movieService: MovieService, private router: Router) {
+    addIcons({ diceOutline });
+  }
 
   ngOnInit() {
     this.loadMovies();
@@ -62,15 +75,21 @@ export class DiscoverPage {
   }
 
 
-
   //Al hacer click en una película:
-  //navegamos a la página de detalles pasando el ID por queryParams
+  //navegamos a la pág de detalles pasando el ig por queryparams
   verFicha(movie: any) {
-    this.router.navigate(
-      ['/movie-details'],
-      { queryParams: { id: movie.id } }
-    );
+    if (!movie) {
+      return;
+    }
+
+    const movieId = movie.id;
+    this.surpriseMovie = null;
+
+    this.router.navigate(['/movie-details'], {
+      queryParams: { id: movieId }
+    });
   }
+
 
   // Carga la siguiente página de películas
   async loadMoreMovies() {
@@ -80,7 +99,53 @@ export class DiscoverPage {
 
 
   irAMoodSearch(mood: string) {
-  this.router.navigate(['/tabs/search'], { queryParams: { mood } });
-}
+    this.router.navigate(['/tabs/search'], { queryParams: { mood } });
+  }
+
+
+  
+  async sorprendeme() {
+    //si ya estamos buscando una peli, salimos para evitar clics dobles
+    if (this.isSurprising) {
+      return;
+    }
+
+    const moods = ['feliz', 'triste', 'emocionado', 'relajado', 'miedo'];
+    const moodAleatorio = moods[Math.floor(Math.random() * moods.length)];
+    const generos = this.moodToGenres[moodAleatorio].join(',');
+    const paginaAleatoria = Math.floor(Math.random() * 5) + 1;
+
+    try {
+      this.isSurprising = true; //bloqueamos el botn
+
+      const res = await this.movieService.discoverMovies({
+        with_genres: generos,
+        'vote_count.gte': '100',
+        include_adult: 'false',
+        page: paginaAleatoria
+      });
+
+      const validas = res.results.filter((m: any) =>
+        m.poster_path && m.vote_average > 0 && m.overview
+      );
+
+      if (validas.length > 0) {
+        //elegimos una peli al azar de las válidas
+        this.surpriseMovie = validas[Math.floor(Math.random() * validas.length)];
+      }
+    }
+    catch (error) {
+      console.error('Error al sorprender:', error);
+    }
+    finally {
+      // Liberamos el botón tanto si sale bien como si hay error
+      this.isSurprising = false;
+    }
+  }
+
+
+  cerrarSorpresa() {
+    this.surpriseMovie = null;
+  }
 
 }
