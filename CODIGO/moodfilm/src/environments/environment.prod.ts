5a60fcc90fd0304f559bec8110a16cccf1fe26c7
martadurangemes <marta.duran.gemes.alu@iesjulioverne.es>
2026-05-19 16:52:29 +0200

    Ignorar cache de Angular

---
+++ CODIGO/moodfilm/src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  //tmdbApiKey: '...',  usada en el backend
+  //tmdbBaseUrl: 'https://api.themoviedb.org/3',
+  tmdbImageUrl: 'https://image.tmdb.org/t/p/w500'
+};
---
+++ CODIGO/moodfilm/src/environments/environment.ts
@@ -0,0 +1,9 @@
+
+//Api de películas
+export const environment = {
+  production: false,
+  //tmdbApiKey: '...',     //clave que me proporciona la api, se usa en el backend
+  //tmdbBaseUrl: 'https://api.themoviedb.org/3', 
+  apiBaseUrl: 'http://localhost:8080',
+  tmdbImageUrl: 'https://image.tmdb.org/t/p/w500'
+};
